f891598df82e4e4d5d84da71a9b375b986274a70
TheTelesto <102152204+TheTelesto@users.noreply.github.com>
2022-09-19 21:57:28 +1000

    commit

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 let images = [];
-track
+track = []
 function preload(){
-
-images[0]= loadImage("images/grass.png");
-images[1]= loadImage("images/track.png");
-
-
-
+images[0]= loadImage("grass.png");
+images[1]= loadImage("track.png");
+track = loadStrings("track.txt");
 }
 function setup() {
-  createCanvas(1000,1000);
-  background("white");
-
+  createCanvas(2000,2000);
+  background("grey");
+  for (let i = 0; i < track.length; i++) {
+   if (track[i] == "1") {
+    image(images[1], i*100, 0, 100, 100);
+   } else {
+    image(images[0], i*100, 0, 100, 100);
+   }
+  }
 }
 
 function draw() {
