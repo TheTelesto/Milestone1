56549da0f60c1e27322a2de073b764bd76eb6464
TheTelesto <102152204+TheTelesto@users.noreply.github.com>
2022-09-19 20:50:53 +1000

    commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let images = [];
+track
+function preload(){
+
+images[0]= loadImage("images/grass.png");
+images[1]= loadImage("images/track.png");
+
+
+
+}
+function setup() {
+  createCanvas(1000,1000);
+  background("white");
+
+}
+
+function draw() {
+
+}
+
